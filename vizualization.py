dd37348cf5057b9f3f9d0f8d9f207b40f0eb416c
Ivan T. Ivanov <iivanov.xz@gmail.com>
2016-08-29 22:25:43 +0300

    use os.path.join for paths

--- vizualization.py
+++ vizualization.py
@@ -1,10 +1,16 @@
 import folium
+import os
 
+from folium import plugins
 
 def vizualization(data, file_name):
     heatmap_map = folium.Map(data[0], zoom_start=20)
 
-    hm = folium.plugins.HeatMap(data)
+    hm = plugins.HeatMap(data)
     heatmap_map.add_children(hm)
-    path = str('maps\\' + file_name + '.html')
+
+    if not os.path.exists('maps'):
+        os.makedirs('maps')
+
+    path = os.path.join('maps', file_name + '.html')
     heatmap_map.save(path)
