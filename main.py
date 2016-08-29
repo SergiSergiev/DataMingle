ab23610b63c3ac7428d5ce1eb85e69d4a4aca259
Ivan T. Ivanov <iivanov.xz@gmail.com>
2016-08-29 21:50:58 +0300

    PyCharm reformat code according PEP8

--- main.py
+++ main.py
@@ -6,51 +6,38 @@
 5. Convert to geo-coordinates
 6. Save on DB
 '''
-import iso8601
-from pprint import pprint
 
-from load_data import load_data, load_sensor_locations
 from data_manipulations import create_time_frames, report, create_list
+from load_data import load_data, load_sensor_locations
 from vizualization import vizualization
 
-from datetime import datetime
 hours = '23:59:59.000'
 approx_in_secs = 10
-routers_number = (57,58,59,60,61,62,63,64,65,66)
+routers_number = (57, 58, 59, 60, 61, 62, 63, 64, 65, 66)
 requested_date = "2016-08-21"
 name_file_outpup = 'bricolage'
 
-def main():
 
+def main():
     db_records_sensor_date = load_data(routers_number, requested_date, hours)
     routers_coordinates = load_sensor_locations(routers_number)
     round_by_sec = create_list(db_records_sensor_date, approx_in_secs)
     time_frames_records = create_time_frames(round_by_sec)
-#    pprint(routers_coordinates)
-#    pprint(time_frames_records)
+    #    pprint(routers_coordinates)
+    #    pprint(time_frames_records)
     report(time_frames_records)
 
     counter = 0
     gathered_coordinates = list()
     for n in time_frames_records:
         if n[26] != None:
-            counter +=1
+            counter += 1
             gathered_coordinates.append(n[-2:])
-    #pprint(gathered_coordinates)
+    # pprint(gathered_coordinates)
     print("Number of coordinates: {}".format(counter))
     print(gathered_coordinates)
-    file_name = name_file_outpup +'_' + requested_date + '_' +str(approx_in_secs)
+    file_name = name_file_outpup + '_' + requested_date + '_' + str(approx_in_secs)
     vizualization(gathered_coordinates, file_name)
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
