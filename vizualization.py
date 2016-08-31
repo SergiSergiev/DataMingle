4701e9c351a5a2bc94f04503e4d91f856cbe8400
Ivan T. Ivanov <iivanov.xz@gmail.com>
2016-08-31 14:23:07 +0300

    add read csv file helper

--- vizualization.py
+++ vizualization.py
@@ -16,6 +16,21 @@ def write_file(file_name, csv_out_lines):
             print(csv_err)
 
 
+def read_file(file_name, display=False):
+    csv_lines = []
+    with open(file_name, 'r', encoding='utf-8') as in_file:
+        in_csv = csv.reader(in_file, escapechar="'", quotechar="'", quoting=csv.QUOTE_NONE)
+
+        try:
+            for index, csv_line in enumerate(in_csv):
+                if display:
+                    print('{:2}: {}'.format(index, csv_line))
+                csv_lines.append(csv_line)
+        except csv.Error as csv_err:
+            print(csv_err)
+    return csv_lines
+
+
 def vizualization(data, file_name):
     heatmap_map = folium.Map(data[0], zoom_start=20)
 
