a8158419dc2c96d82b7c55c3b94eec245b70e48b
Ivan T. Ivanov <iivanov.xz@gmail.com>
2016-09-09 09:41:08 +0300

    split circles drawing out of index page
    Use /circles endpoint for circles visualization.
    Added 'sec' paramter, which control 'sensor-frame' interval.
    Separate paramters reading and DB data load to functions.

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from flask import render_template, request, session
 from werkzeug.contrib.cache import SimpleCache
 from app import app
 from datetime import datetime, time
-import platform
+import os
 import bricolage, pickle
 from filtering import trilaterate, round_seconds, segregate_average
 from dbload import load_data, load_sensor_locations
@@ -29,60 +29,7 @@ class cached(object):
         return decorator
 
 
-def load_db_samples(start_date, current_hour, sensors_ids):
-    start_date_time = datetime.combine(start_date, time(current_hour, 0, 0))
-    end_date_time = datetime.combine(start_date, time(current_hour, 59, 0))
-    db_records = load_data(sensors_ids, start_date_time, end_date_time)
-
-    print('{:10} database records'.format(len(db_records)))
-
-    return db_records
-
-
-def compute_heatmap(db_records, sensors_ids):
-    sensor_points = load_sensor_locations(sensors_ids)
-    approx_in_secs = 10
-
-    zones = bricolage.get_zones(10, 10)
-    borders = bricolage.get_borders()
-
-    round_by_sec = round_seconds(db_records, approx_in_secs)
-
-    adjusted = []
-    outside = []
-    sensor_frames = segregate_average(round_by_sec)
-    circles, coordinates = trilaterate(sensor_frames, sensor_points)
-    for point in coordinates:
-        point_fit = False
-        for zone in zones:
-            if zone.contain(point):
-                point_fit = True
-                zone.visit()
-                adjusted.append(zone)
-        if not point_fit:
-            outside.append(point)
-
-    print("{:10} coordinates".format(len(coordinates)))
-    print("{:10} adjusted coordinates".format(len(adjusted)))
-    print('{:10} points outside the grid'.format(len(outside)))
-
-    heatmap = []
-    # for a in adjusted:
-    #     heatmap.append((a.m.lat, a.m.lon, a.visited))
-
-    for p in coordinates:
-        heatmap.append((p.lat, p.lon, 1))
-
-    return circles, heatmap
-
-
-@app.route('/', methods=['GET'])
-@app.route('/index', methods=['GET'])
-def index():
-    host_name = platform.node()
-    sensors_ids = (57, 58, 59, 60, 61, 62, 63, 64, 65, 66)
-    auto_refresh = True if host_name != 'sid-hdb' else False
-
+def parse_parameters():
     date_string = request.args.get('date', None)
     if date_string is not None:
         try:
@@ -104,6 +51,15 @@ def index():
     if start_date is None:
         start_date = datetime.strptime('2016-08-12', "%Y-%m-%d")
 
+    sec_sting = request.args.get('sec', None)
+    if sec_sting is not None:
+        try:
+            approx_in_secs = int(sec_sting)
+            session['approx_in_secs'] = approx_in_secs
+        except Exception as ex:
+            print('{}: {}'.format(ex, sec_sting))
+
+
     current_hour = session.get('current_hour', None)
     if current_hour is None:
         current_hour = 8
@@ -111,8 +67,17 @@ def index():
     if current_hour > 23:
         current_hour = 8
 
+    approx_in_secs = session.get('approx_in_secs', None)
+    if approx_in_secs is None:
+        approx_in_secs = 10
+
+    return start_date, current_hour, approx_in_secs
+
+
+def load_db_samples(start_date, current_hour, sensors_ids):
     db_records = None
-    db_records_name = 'db-{}-{}.pickle'.format(str(start_date), str(current_hour))
+    db_file_name = 'db-{}-{}.pickle'.format(str(start_date), str(current_hour))
+    db_records_name = os.path.join('tmp', db_file_name)
 
     try:
         with open(db_records_name, 'rb') as pickle_file:
@@ -123,28 +88,95 @@ def index():
     print('{}'.format(datetime.combine(start_date, time(current_hour, 0, 0))))
 
     if db_records is None:
-        db_records = load_db_samples(start_date, current_hour, sensors_ids)
+        start_date_time = datetime.combine(start_date, time(current_hour, 0, 0))
+        end_date_time = datetime.combine(start_date, time(current_hour, 59, 0))
+        db_records = load_data(sensors_ids, start_date_time, end_date_time)
         with open(db_records_name, 'wb') as pickle_file:
             pickle.dump(db_records, pickle_file)
 
-    circles, heatmap = compute_heatmap(db_records, sensors_ids)
+    print('{:10} database records'.format(len(db_records)))
 
-    if auto_refresh:
-        heatmap = None
-        current_circles = session.get('current_circles', None)
+    return db_records
 
-        if current_circles is None or current_circles >= len(circles):
-            current_circles = 0
 
-        circle_list = circles[current_circles]
-        current_circles += 1
-        session['current_circles'] = current_circles
-    else:
-        current_hour += 1
-        circle_list = None
+def compute_heatmap(db_records, sensors_ids, approx_in_secs):
+    sensor_points = load_sensor_locations(sensors_ids)
+
+    zones = bricolage.get_zones(10, 10)
+
+    round_by_sec = round_seconds(db_records, approx_in_secs)
+
+    adjusted = []
+    outside = []
+    sensor_frames = segregate_average(round_by_sec)
+    circles, coordinates = trilaterate(sensor_frames, sensor_points)
+    for point in coordinates:
+        point_fit = False
+        for zone in zones:
+            if zone.contain(point):
+                point_fit = True
+                zone.visit()
+                adjusted.append(zone)
+        if not point_fit:
+            outside.append(point)
+
+    print('{:10} seconds approximation'.format(approx_in_secs))
+    print("{:10} coordinates".format(len(coordinates)))
+    print("{:10} adjusted coordinates".format(len(adjusted)))
+    print('{:10} points outside the grid'.format(len(outside)))
+
+    heatmap = []
+    # for a in adjusted:
+    #     heatmap.append((a.m.lat, a.m.lon, a.visited))
+
+    for p in coordinates:
+        heatmap.append((p.lat, p.lon, 1))
+
+    return circles, heatmap
+
+
+@app.route('/circles', methods=['GET'])
+def circles():
+    sensors_ids = (57, 58, 59, 60, 61, 62, 63, 64, 65, 66)
+
+    start_date, current_hour, approx_in_secs = parse_parameters()
+
+    db_records = load_db_samples(start_date, current_hour, sensors_ids)
+
+    circles, _ = compute_heatmap(db_records, sensors_ids, approx_in_secs)
+
+    current_circles = session.get('current_circles', None)
+
+    if current_circles is None or current_circles >= len(circles):
+        current_circles = 0
+
+    circle_list = circles[current_circles]
+    current_circles += 1
+    session['current_circles'] = current_circles
 
     session['start_date'] = start_date
     session['current_hour'] = current_hour
+    session['approx_in_secs'] = approx_in_secs
 
-    return render_template("index.html", points=heatmap, circles=circle_list, auto_refresh=auto_refresh)
+    return render_template("index.html", points=None, circles=circle_list, auto_refresh=True)
+
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    sensors_ids = (57, 58, 59, 60, 61, 62, 63, 64, 65, 66)
+
+    start_date, current_hour, approx_in_secs = parse_parameters()
+
+    db_records = load_db_samples(start_date, current_hour, sensors_ids)
+
+    _, heatmap = compute_heatmap(db_records, sensors_ids, approx_in_secs)
+
+    current_hour += 1
+
+    session['start_date'] = start_date
+    session['current_hour'] = current_hour
+    session['approx_in_secs'] = approx_in_secs
+
+    return render_template("index.html", points=heatmap, circles=None, auto_refresh=False)
 
